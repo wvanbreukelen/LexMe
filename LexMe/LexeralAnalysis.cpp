b0d192e32e8c28f09b51f30d616bda8e53c0a6ce
wvanbreukelen <wdownload@xs4all.nl>
2018-08-19 18:10:22 +0200

    Some commenting...

--- LexMe/LexeralAnalysis.cpp
+++ LexMe/LexeralAnalysis.cpp
@@ -2,11 +2,12 @@
 #include "LexeralAnalysis.h"
 
 
-LexeralAnalysis::LexeralAnalysis()
-{
-}
+TokenList LexeralAnalysis::processString(std::string str) {
+	// First, we need to check with with kind of character we are dealing with.
 
+	// Then, we need to extract the data required for this token, if required.
 
-LexeralAnalysis::~LexeralAnalysis()
-{
+	// Finally, we will create a new Token and add it to our token list
+
+	// We repeat this process to the end of the string. We throw exceptions if things go wrong.
 }
--- LexMe/LexeralAnalysis.h
+++ LexMe/LexeralAnalysis.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <string>
 #include "LanguageDefinition.h"
+#include "TokenList.h"
 
 class LexeralAnalysis
 {
 public:
 	LexeralAnalysis();
 	~LexeralAnalysis();
+
+	TokenList processString(std::string str);
 };
 
